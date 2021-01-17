44e190c18f70ef651e9eab8c88dd1f09ac07b387
yusa0730 <isizawa5226@gmail.com>
2021-01-18 02:11:43 +0900

    axiosの実装

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,34 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <ul>
+      <li v-for="memo in memos" :key="memo.id">
+        {{ memo.title }} : {{ memo.description }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+// axiosというモジュールをインポートしています。
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      memos: "memos"
+    }
+  },
+  mounted () {
+    this.setMemo();
+  },
+  methods: {
+    setMemo: function() {
+      axios.get('/api/memos')
+      // .then()で通信が成功した際の処理を書きます。
+      .then(response => (
+        // Axiosで呼び出したAPIの情報をmemosに代入しています。
+        this.memos = response.data
+      ))
     }
   }
 }
