2882e694bf32113482bf8248ec35f87683f360c8
yusa0730 <isizawa5226@gmail.com>
2021-01-19 04:06:39 +0900

    jbuilderを利用してcreateメソッドで作成したAPIを取得

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -4,4 +4,23 @@ class Api::MemosController < ApplicationController
     # つまり、order('created_at DESC') というコードで、created_atが降順(データが作成された順番)に並び替えています。
     @memos = Memo.order("created_at Desc")
   end
+
+  def create
+    @memo = Memo.create(memo_params)
+    # saveメソッドは保存がうまくいったらtrue、保存に失敗したらfalseを返します。
+    # status: :created や status: :unprocessable_entity でステータスを付与しています。
+    # ステータスを付与することで処理が成功したり、失敗したことを示すことができます。
+    if @memo.save
+      render :show, status: :created
+    else
+      render json: @memo.errors, status: :unprocessable_entity
+    end
+  end
+
+  private 
+    def memo_params
+      # paramsとは送られてきたリクエスト情報をひとまとめにしたものです。
+      # permitで変更可能なキーを指定します。今回の場合、titleキーとdescriptionキーを指定しています。
+      params.permit(:title, :description)
+    end
 end
---
+++ app/views/api/memos/show.json.jbuilder
@@ -0,0 +1,6 @@
+# [例]json.キー インスタンス変数.キー
+# 今回のコードの場合、以下のようなjsonデータを返します。
+# {"title": "メモタイトル1", "description": "メモ詳細1"}
+ 
+json.title @memo.title
+json.description @memo.description
