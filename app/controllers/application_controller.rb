681ee521e99be1794770fcd11db3e858e609e382
yusa0730 <isizawa5226@gmail.com>
2021-01-19 23:42:15 +0900

    csrfの無効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  # CSRF対策を無効にするには、app/controllers/application_controller.rbに以下のコードを追加します。
+  # https://techracho.bpsinc.jp/hachi8833/2017_10_23/46891
+  protect_from_forgery
 end
